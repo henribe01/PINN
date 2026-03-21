715a3830ac7b019447f466718210e2e52efeef25
Henri Benecke <bba3815@studium.uni-hamburg.de>
2026-03-21 14:09:55 +0100

    Enhance pendulum simulation with training progress bar and evaluation saving; update README for PINNs overview and advantages

--- pendulum.py
+++ pendulum.py
@@ -1,8 +1,10 @@
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import torch.nn as nn
 import torch
+from progress.bar import Bar
 
 class PendulumMLP(nn.Module):
     def __init__(self):
@@ -22,52 +24,70 @@ class PendulumMLP(nn.Module):
         output = self.output_layer(h4)
         return output
     
-def train_model(model, t, true_angles, t_physics, num_epochs=1000, learning_rate=0.001, lambda_physical = 0.1):
+def train_model(model, t, true_angles, t_physics, t_eval, num_epochs=1000, learning_rate=0.001, lambda_physical = 0.1):
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.MSELoss()
     loss_history = np.zeros(num_epochs)
     
-    for epoch in range(num_epochs):
-        model.train()
-        optimizer.zero_grad()
-        
-        # Compute the MSE loss
-        predicted_angles = model(t)
-        mse_loss = loss_fn(predicted_angles, true_angles)
-        
-        # Compute the physical loss using automatic differentiation
-        predicted_physics = model(t_physics)
-        dtheta_dt = torch.autograd.grad(
-            outputs=predicted_physics, 
-            inputs=t_physics,
-            grad_outputs=torch.ones_like(predicted_physics),
-            create_graph=True,
-            retain_graph=True
-        )[0]
-        domega_dt = torch.autograd.grad(
-            outputs=dtheta_dt, 
-            inputs=t_physics,
-            grad_outputs=torch.ones_like(dtheta_dt),
-            create_graph=True,
-            retain_graph=True
-        )[0]
-        # Calculate the physical loss based on the pendulum equation
-        physical_residual = domega_dt + (9.81 / 1.0) * torch.sin(predicted_physics)
-        physical_loss = torch.mean(physical_residual ** 2)
-        # Total loss is a combination of MSE loss and physical loss
-        total_loss = mse_loss + lambda_physical * physical_loss
-        
-        # Backward pass and optimization
-        total_loss.backward()
-        optimizer.step()
-        
-        # Print the loss every 100 epochs
-        if epoch % 100 == 0:
-            print(f"Epoch {epoch}, Total Loss: {total_loss.item():.4f}, MSE Loss: {mse_loss.item():.4f}, Physical Loss: {physical_loss.item():.4f}")
-        loss_history[epoch] = total_loss.item()
-        
-        if t_physics.grad is not None:
-            t_physics.grad.zero_()  # Clear the gradients for the next iteration
+    with Bar('Training', max=num_epochs) as bar:
+        for epoch in range(num_epochs):
+            model.train()
+            optimizer.zero_grad()
+            
+            # Compute the MSE loss
+            predicted_angles = model(t)
+            mse_loss = loss_fn(predicted_angles, true_angles)
+            
+            # Compute the physical loss using automatic differentiation
+            predicted_physics = model(t_physics)
+            dtheta_dt = torch.autograd.grad(
+                outputs=predicted_physics, 
+                inputs=t_physics,
+                grad_outputs=torch.ones_like(predicted_physics),
+                create_graph=True,
+                retain_graph=True
+            )[0]
+            domega_dt = torch.autograd.grad(
+                outputs=dtheta_dt, 
+                inputs=t_physics,
+                grad_outputs=torch.ones_like(dtheta_dt),
+                create_graph=True,
+                retain_graph=True
+            )[0]
+            # Calculate the physical loss based on the pendulum equation
+            physical_residual = domega_dt + (9.81 / 1.0) * torch.sin(predicted_physics)
+            physical_loss = torch.mean(physical_residual ** 2)
+            # Total loss is a combination of MSE loss and physical loss
+            total_loss = mse_loss + lambda_physical * physical_loss
+            
+            # Backward pass and optimization
+            total_loss.backward()
+            optimizer.step()
+            
+            # Print the loss every 1000 epochs
+            # if epoch % 1000 == 0:
+            #     print(f"Epoch {epoch}, Total Loss: {total_loss.item():.4f}, MSE Loss: {mse_loss.item():.4f}, Physical Loss: {physical_loss.item():.4f}")
+            loss_history[epoch] = total_loss.item()
+            
+            # Eval and save the predicted angles for plotting every 200 epochs
+            if epoch % 200 == 0:
+                model.eval()
+                with torch.no_grad():
+                    predicted_angles_eval = model(t_eval).squeeze().numpy()
+                    path = f"saves_without_physical_loss/pendulum_epoch_{epoch}.npy"
+                    np.save(path, predicted_angles_eval)
+                    # print(f"Saved predicted angles at epoch {epoch} to {path}")
+            
+            if t_physics.grad is not None:
+                t_physics.grad.zero_()  # Clear the gradients for the next iteration
+                
+            bar.next()
+
+    model.eval()
+    with torch.no_grad():
+        predicted_angles_eval = model(t_eval).squeeze().numpy()
+        np.save(f"saves_without_physical_loss/pendulum_epoch_{num_epochs}.npy", predicted_angles_eval)
+        # print("Saved final predicted angles to saves/pendulum_final.npy")
 
     return loss_history
 
@@ -80,6 +100,9 @@ if __name__ == "__main__":
     omega0 = 0.0        # Initial angular velocity
     t_span = (0, 10)    # Time span for the simulation
     t_eval = np.linspace(t_span[0], t_span[1], 10000)  # Time points to evaluate
+    seed = 42
+    np.random.seed(seed)
+    torch.manual_seed(seed)
     
     # Define the pendulum dynamics
     def pendulum_dynamics(t, y):
@@ -93,15 +116,21 @@ if __name__ == "__main__":
     true_angles = sol.y[0]  # Extract the angles from the solution
     
     # Pick a subset of the data for training
-    n_samples = 100
+    n_samples = 20
     indices = np.random.choice(len(t_eval), n_samples, replace=False)
     t_train = torch.tensor(t_eval[indices], dtype=torch.float32).unsqueeze(1)  # Used for the MSE loss, shape (n_samples, 1)
     true_angles_train = torch.tensor(true_angles[indices], dtype=torch.float32).unsqueeze(1)  # Used for the MSE loss, shape (n_samples, 1)
-    t_physics = torch.linspace(t_span[0], t_span[1], 5000, dtype=torch.float32, requires_grad=True).unsqueeze(1)  # Used for the physical loss, shape (1000, 1)    
+    t_physics = torch.linspace(t_span[0], t_span[1], 5000, dtype=torch.float32).unsqueeze(1).requires_grad_(True)  # Used for the physical loss, shape (1000, 1)
+    t_eval_tensor = torch.tensor(t_eval, dtype=torch.float32).unsqueeze(1)  # Used for evaluation, shape (10000, 1)
+    
+    # Add some noise to the training data to make it more realistic
+    noise_std = 0.05
+    true_angles_train += noise_std * torch.randn_like(true_angles_train)
     
     # Initialize the model and train it
+    num_epochs = 6000
     model = PendulumMLP()
-    loss_history = train_model(model, t_train, true_angles_train, t_physics, num_epochs=4000, learning_rate=0.001, lambda_physical=0.1)
+    loss_history = train_model(model, t_train, true_angles_train, t_physics, t_eval_tensor, num_epochs=num_epochs, learning_rate=0.001, lambda_physical=0)
     
     # Plot the loss history
     fig, ax = plt.subplots()
@@ -113,17 +142,25 @@ if __name__ == "__main__":
     ax.grid(zorder=0)
     ax.set_xlim(0, len(loss_history))
     
-    # Plot the true angles and the predicted angles
-    model.eval()
-    with torch.no_grad():
-        predicted_angles = model(torch.tensor(t_eval, dtype=torch.float32).unsqueeze(1)).squeeze().numpy()
-    fig, ax = plt.subplots()
-    ax.plot(t_eval, true_angles, label='True Angles', color='black', linestyle='dashed', alpha=0.7, zorder=4)
-    ax.plot(t_eval, predicted_angles, label='Predicted Angles', color='red', zorder=5)
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Angle (rad)')
-    ax.set_title('Pendulum Angle Over Time')
-    ax.legend()
-    ax.grid(zorder=0)
-    ax.set_xlim(t_span)
+    # Create an animation of the predicted angles over time
+    fig2, ax2 = plt.subplots()
+    ax2.set_xlim(t_span)
+    ax2.set_ylim(-1.5, 1.5)
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Angle (rad)')
+    ax2.set_title('Pendulum Angle Over Time')
+    line, = ax2.plot([], [], label='Predicted Angle', color='red')
+    scat = ax2.scatter(t_train.numpy(), true_angles_train.numpy(), label='Training Data', color='black', zorder=5, marker='x')
+    true_line, = ax2.plot(t_eval, true_angles, label='True Angle', color='black', linestyle='dashed', zorder=4, alpha=0.7)
+    ax2.legend()
+    ax2.grid(zorder=0)
+    
+    def update(frame):
+        predicted_angles = np.load(f"saves_without_physical_loss/pendulum_epoch_{frame}.npy")
+        line.set_data(t_eval, predicted_angles)
+        return line,
+    
+    anim = FuncAnimation(fig2, update, frames=range(0, num_epochs + 1, 200), blit=True)
+    anim.save('pendulum_animation_without_physical_loss.gif', writer='pillow', fps=5)
     plt.show()
+    
--- readme.md
+++ readme.md
@@ -5,3 +5,15 @@ This repository contains implementations of Physics-Informed Neural Networks (PI
 - [ ] Get a basic understanding of PINNs and their applications.
 - [ ] Implement a simple PINN to solve a basic differential equation.
 - [ ] Explore more complex problems and applications of PINNs in the future.
+
+## PINNs Overview
+Physics-Informed Neural Networks (PINNs) are a class of neural networks that incorporate physical laws into the training process. They are designed to solve differential equations by minimizing a loss function that includes both data fitting and the residual of the differential equation.
+### Advantages of PINNs
+- Can solve complex differential equations without the need for mesh generation.
+- Can handle noisy data and lack of data.
+### Examples of Applications
+My first implementation is a PINN for solving the pendulum. First, I generate some data by solving the pendulum equation using a numerical method, pick $20$ random points from the generated data, and add some noise to it to simulate real-world sensor data. Then, I train a neural network to learn the underlying physics of the pendulum motion by minimizing the loss function that includes both the data fitting and the residual of the pendulum equation.
+![PINN for Pendulum](pendulum_animation_with_physical_loss.gif)
+We can see that the PINN is able to learn the underlying physics of the pendulum motion and can predict the future states of the pendulum accurately, even with noisy data. If we remove the physical loss from the training process, the neural network struggles to learn the correct dynamics of the pendulum, resulting in overfitting to the noisy data.
+![PINN for Pendulum without Physical Loss](pendulum_animation_without_physical_loss.gif)
+This demonstrates the importance of incorporating physical laws into the training process of neural networks, especially when dealing with noisy data or limited data. 
